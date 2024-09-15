2bee6b19eb6e32fa1168c60fbc5e52bd242c9a47
deewakar05 <150002353+deewakar05@users.noreply.github.com>
2024-09-15 16:18:42 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++) cin>>arr[i];
+    int target;
+    cin>>target;
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            cout<<i+1;
+        }
+    }
+
+}
